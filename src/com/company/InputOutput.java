07fe1a0b06fc8f76db6f1cc08b654d6928bc31b3
сачек денис <sachek.denis@yandex.by>
2019-02-26 19:44:32 +0300

    111

--- src/com/company/InputOutput.java
+++ src/com/company/InputOutput.java
@@ -1,16 +1,18 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.nio.Buffer;
 
 public class InputOutput
     {
-       Experiment[] Read()
+       void Read()
        {
-           try(BufferedReader bf = new BufferedReader(new FileReader("1.txt"))
-           {
-
-           }
+           FileReader fr = new FileReader("./1.txt");
+           //try(BufferedReader bf = new BufferedReader(new FileReader("C:\\Users\\User\\IdeaProjects\\Lab2\\src\\1.txt")))
+           //{
+//
+  //         }
        }
     }
